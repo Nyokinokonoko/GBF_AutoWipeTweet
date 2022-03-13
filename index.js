6db841690eae0debda45caa562f83268f658473e
Kenny Ha <77214322+altkennyh2l@users.noreply.github.com>
2022-03-14 08:34:53 +0900

    Checking 5 latest tweets instead of 1. Logging time.

--- index.js
+++ index.js
@@ -20,23 +20,73 @@ async function getLatestTweet(target) {
     }
   );
   const tweet = await gettingTweetResponse.json();
-  let result = [tweet.data[0].id, tweet.data[0].source];
+  let result = [
+    tweet.data[0].id,
+    tweet.data[0].source,
+    tweet.data[1].id,
+    tweet.data[1].source,
+    tweet.data[2].id,
+    tweet.data[2].source,
+    tweet.data[3].id,
+    tweet.data[3].source,
+    tweet.data[4].id,
+    tweet.data[4].source,
+  ];
   return result;
 }
 
-async function deleteGBFTweet() {
-  let newestTweet = await getLatestTweet(process.env.TARGET_USERID);
-  if (newestTweet[1] === target_source) {
-    T.post(
-      "statuses/destroy/:id",
-      { id: newestTweet[0] },
-      function (err, data, response) {
-        if (err) {
-          console.log(err);
-        }
+async function deleteGBFTweet(targetTweetID) {
+  T.post(
+    "statuses/destroy/:id",
+    { id: targetTweetID },
+    function (err, data, response) {
+      if (err) {
+        console.log(err);
       }
-    );
-  }
+    }
+  );
 }
 
-let runEvery30Second = setInterval(() => deleteGBFTweet(), 30000);
+async function checkLast5Tweets() {
+  let checkTarget = await getLatestTweet(process.env.TARGET_USERID);
+  let tweetIDs = [
+    checkTarget[0],
+    checkTarget[2],
+    checkTarget[4],
+    checkTarget[6],
+    checkTarget[8],
+  ];
+  let sources = [
+    checkTarget[1],
+    checkTarget[3],
+    checkTarget[5],
+    checkTarget[7],
+    checkTarget[9],
+  ];
+
+  for (let i = 0; i < 5; i++) {
+    if (sources[i] === target_source) {
+      await deleteGBFTweet(tweetIDs[i]);
+      console.log(`Deleted tweet by GBF. (ID: ${tweetIDs[i]})`);
+    }
+  }
+
+  let currentDate = new Date();
+  let dateTime =
+    "Last Check: " +
+    currentDate.getDate() +
+    "/" +
+    (currentDate.getMonth() + 1) +
+    "/" +
+    currentDate.getFullYear() +
+    " @ " +
+    currentDate.getHours() +
+    ":" +
+    currentDate.getMinutes() +
+    ":" +
+    currentDate.getSeconds();
+
+  console.log(`Finished checking the latest 5 tweets. (${dateTime})`);
+}
+
+let runEvery30Second = setInterval(() => checkLast5Tweets(), 30000);
